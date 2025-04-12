36be74b1317a4ab06931fa679ffb0e783941323e
Deyson Urrego <deysonurrego159@gmail.com>
2025-04-11 21:10:52 -0500

    Prueba de conexion exitosa

--- database/probar_con.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'database/conexion.php'; // Incluye la clase
-
-$db = new Database();                 // Crea una instancia
-$conexion = $db->getConnection();    // Llama al método
-var_dump($conexion);                 // Imprime la conexión para comprobarla
